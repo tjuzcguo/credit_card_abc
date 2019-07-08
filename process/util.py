03d0c2ae7f5ceaa6f3a7b48a1a99d334a49011b7
Guozongchao <gzongchao1@163.com>
2019-07-08 20:28:39 +0800

    Create util.py

---
+++ process/util.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
+import seaborn as sns
+from scipy import stats
+import copy
+
+
+def mono_bin(Y, X, n=10):
+    r = 0
+    good = Y.sum()
+    bad = Y.count() - good
+    while np.abs(r) < 1:
+        d1 = pd.DataFrame({"X": X, "Y": Y, "Bucket": pd.qcut(X, n)})
+        d2 = d1.groupby('Bucket', as_index=True)
+        r, p = stats.spearmanr(d2.mean().X, d2.mean().Y)
+        n = n - 1
+    d3 = pd.DataFrame(d2.X.min(), columns=['min'])
+    d3['min'] = d2.min().X
+    d3['max'] = d2.max().X
+    d3['sum'] = d2.sum().Y
+    d3['total'] = d2.count().Y
+    d3['rate'] = d2.mean().Y
+    d3['woe'] = np.log((d3['rate'] / good) / ((1 - d3['rate']) / bad))
+    d3['goodattribute'] = d3['sum'] / good
+    d3['badattribute'] = (d3['total'] - d3['sum']) / bad
+    iv = ((d3['goodattribute'] - d3['badattribute']) * d3['woe']).sum()
+    d4 = (d3.sort_values(by='min')).reset_index(drop=True)
+    woe = list(d4['woe'].round(3))
+    cut = []
+    cut.append(float('-inf'))
+    for i in range(1, n + 1):
+        qua = X.quantile(i / (n + 1))
+        cut.append(round(qua, 4))
+    cut.append(float('inf'))
+    return d4, iv, cut, woe
+
+
+
