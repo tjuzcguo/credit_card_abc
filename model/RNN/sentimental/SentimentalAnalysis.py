db651fec1a723c769d4ce7876fb9fc7f95850d67
tjuzcguo <gzongchao1@163.com>
2019-08-18 19:26:37 +0800

    add sentimental rnn lstm

---
+++ model/RNN/sentimental/SentimentalAnalysis.py
@@ -0,0 +1,48 @@
+from keras.datasets import imdb
+from keras.preprocessing import sequence
+from keras import Sequential
+from keras.layers import Embedding, LSTM, Dense, Dropout
+
+
+
+vocabulary_size = 5000
+(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words = vocabulary_size)
+print('Loaded dataset with {} training samples, {} test samples'.format(len(X_train), len(X_test)))
+
+
+word2id = imdb.get_word_index()
+id2word = {i: word for word, i in word2id.items()}
+print('---review with words---')
+print([id2word.get(i, ' ') for i in X_train[6]])
+print('---label---')
+print(y_train[6])
+
+
+max_words = 500
+X_train = sequence.pad_sequences(X_train, maxlen=max_words)
+X_test = sequence.pad_sequences(X_test, maxlen=max_words)
+
+print("Building model......")
+embedding_size=32
+model=Sequential()
+model.add(Embedding(vocabulary_size, embedding_size, input_length=max_words))
+model.add(LSTM(100))
+model.add(Dense(1, activation='sigmoid'))
+print(model.summary())
+
+print('Start compile model......')
+model.compile(loss='binary_crossentropy',
+             optimizer='adam',
+             metrics=['accuracy'])
+
+
+batch_size = 64
+num_epochs = 3
+X_valid, y_valid = X_train[:batch_size], y_train[:batch_size]
+X_train2, y_train2 = X_train[batch_size:], y_train[batch_size:]
+model.fit(X_train2, y_train2, validation_data=(X_valid, y_valid), batch_size=batch_size, epochs=num_epochs)
+
+
+print('Start evaluate model......')
+scores = model.evaluate(X_test, y_test, verbose=0)
+print('Test accuracy:', scores[1])
